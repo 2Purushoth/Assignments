284b7c096a2f3dc304d336a9b3b60316c802ea2b
2Purushoth <132731153+2Purushoth@users.noreply.github.com>
2023-05-06 07:00:05 +0100

    Add files via upload
    day 1 phone

---
+++ phone.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class phone {
+	public void gaming() {
+		System.out.println("gaming");
+	}
+	public void watchingMovie() {
+		System.out.println("watchingMovie");
+	}
+	public void calling() {
+		System.out.println("calling");
+	}
+	public void usingSocialMedia() {
+		System.out.println("usingSocialMedia");
+	}
+public static void main(String[]args) {
+	phone phone=new phone();
+	phone.gaming();
+	phone.watchingMovie();
+	phone.calling();
+	phone.usingSocialMedia();
+			
+}
+
+}
+
