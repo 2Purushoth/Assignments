455f75f340ad7e90d6ee093931eac43a0cc59ae7
2Purushoth <132731153+2Purushoth@users.noreply.github.com>
2023-05-08 18:46:51 +0100

    Add files via upload
    day2 Assingment

---
+++ mobile.java
@@ -0,0 +1,21 @@
+package week1.da2;
+
+public class mobile {
+	public static void main(String[]args) {
+		String mobileBrandName="oppo";
+		System.out.println( mobileBrandName);
+		char mobileLogo='P';
+		System.out.println( mobileLogo);
+		short noOfMobilePiece=2;
+		System.out.println(noOfMobilePiece);
+		int modelNumber=12345;
+		System.out.println(modelNumber);
+		long mobileNumber=9876543210l;
+		System.out.println( mobileNumber);
+		float mobilePrice=18.500f;
+		System.out.println( mobilePrice);
+		boolean mobileIsDamaged=true;
+		System.out.println(mobileIsDamaged);
+	}
+
+}
