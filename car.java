028ed0dd0a699ba921f0aef86f15a33e0009653f
2Purushoth <132731153+2Purushoth@users.noreply.github.com>
2023-05-08 18:45:58 +0100

    Add files via upload
    day2 assingment

--- car.java
+++ car.java
@@ -1,28 +1,20 @@
-package week1.day1;
+package week1.da2;
 
 public class car {
-	public void openDoor() {
-		System.out.println("openDoor");
+	public static void main(String[]args) {
+		int noOfWheels=4;
+		System.out.println(noOfWheels);
+		short noOfMirror=2;
+		System.out.println(noOfMirror);
+		long chaseNo=1234567l;
+		System.out.println( chaseNo);
+		boolean tyreIsPuncture=true;
+		System.out.println(tyreIsPuncture);
+		String bikeName="duke";
+		System.out.println(bikeName);
+		double runningKm=89.2;
+		System.out.println(runningKm);
+		
 	}
-	public void fourWheels() {
-		System.out.println("fourWheels");
-	}
-	public void adjustMirror() {
-		System.out.println("adjustMirror");
-	}
-	public void pressBreak() {
-		System.out.println("pressBreak");
-	}
-	public void pressAcclerator()  {
-		System.out.println("pressAcclerator");
-	}
-public static void main(String[]args) {
-	car car=new car();
-	car.openDoor();
-	car.adjustMirror();
-	car.pressBreak();
-	car.pressAcclerator();
-	car.fourWheels();
-}
- 
+
 }
